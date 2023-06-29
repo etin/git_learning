82511e0b638a1cff1fe3a474ed7b5a5144b25b18
Mark <market4my@google.com>
2023-06-29 13:27:25 +0300

    Добавлена функция минус

--- first/nums.py
+++ first/nums.py
@@ -2,5 +2,12 @@ def plus(a, b):
     return a + b
 
 
+text = "new next"   # добавил этот текст (изм в фле nums.py после коммита init)
+
+
+def minus(a, b):
+    return a - b
+
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(minus(4, 2))
